64955a361c02ba6daa30fee3bb7cb4f420abdddd
MubasshirLakhani <mubashirlakhani11@gmail.com>
2023-01-14 17:54:08 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[text,setText]= useState("");
+  const[todos,setTodos]= useState([]);
+  const[IndexNum,setIndexNum]= useState();
+
+
+
+
+  let add=()=>{
+    if(IndexNum > -1){
+    todos[IndexNum]= text
+   setTodos([...todos]);
+
+   setIndexNum(-1);
+  } else{
+    setText("");
+    setTodos([...todos,text]);
+  }
+  };
+
+
+  let deleteTodo=(todoIndex)=>{
+    const todosClone=[...todos];
+    todosClone.splice(todoIndex,1);
+    setTodos(todosClone);
+  };
+
+
+   let editTodo=(i)=>{
+    setIndexNum(i)
+    setText(todos[i]);
+   };
+
+   
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>state</h1>
+       
+       <input 
+       value={text}
+       onChange={(e)=> setText(e.target.value)}
+       placeholder=' enter value'/>
+       <button onClick={()=>add('inp')}>add</button>
+
+       
+       <div>
+        {todos.map((todo,index)=>(
+          <div>
+            <p key={index}>{todo}</p>
+            <button onClick={()=>deleteTodo(index)}>delete</button>
+            <button onClick={()=>editTodo(index)}>edit</button>
+          </div>
+        
+          ))}
+       </div>
       </header>
     </div>
   );
